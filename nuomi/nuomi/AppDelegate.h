956b5fe09398049f5843794faeb9b86effa2b786
M了个M <847881570@qq.com>
2015-12-13 09:35:39 +0800

    初始化代码仓库

---
+++ nuomi/nuomi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nuomi
+//
+//  Created by 周发明 on 15/12/13.
+//  Copyright © 2015年 周发明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
